9acd4743d0b25a6a1d432c687a6ef4476c226c8e
dates6249-dev <dates6249@gmail.com>
2026-01-11 07:39:58 +0300

    Create bot.py
    add bot.py

---
+++ bot.py
@@ -0,0 +1,16 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Бот успешно запущен 🚀")
+
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
